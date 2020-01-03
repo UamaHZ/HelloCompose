ca46c07892ba1eb94002185678fbd7c9cd2ad26e
liwei <liwei@uama.com.cn>
2020-01-03 15:11:23 +0800

    增加预览函数

--- app/src/main/java/cn/com/uama/demo/hellocompose/MainActivity.kt
+++ app/src/main/java/cn/com/uama/demo/hellocompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +29,22 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun Greeting(name: String) {
         Text(text = "Hello $name!")
+    }
+
+    /**
+     * 预览函数
+     * 预览函数可以使你在 IDE 中即可预览可组合函数的效果，而无需运行到手机或模拟器上。
+     * 预览函数主要的限制是不能有参数，所以就不能对 Greeting(name) 函数进行预览。
+     * 因此需要再定义一个无参的 PreviewGreeting() 函数作为预览函数，在预览函数中调用 Greeting(name) 函数并传输适当的参数。
+     * 由于一个可组合函数必须在另一个可组合函数中被调用，所以预览函数也必须是可组合函数。
+     * 在 @Composable 注解前面再加一个 @Preview 注解即表示这是一个预览函数。
+     *
+     * 重新构件工程，就可以看到一个预览窗口，窗口中展示的是预览函数中描述的 UI 元素。
+     * 当代码有变动是，预览窗口不会自动刷新，需要手动点击刷新按钮或者重新构件工程。
+     */
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting(name = "Compose")
     }
 }
