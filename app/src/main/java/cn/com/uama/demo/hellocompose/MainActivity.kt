a399db467674c0a589a0137c1c0b9abad3b44943
liwei <liwei@uama.com.cn>
2020-01-03 14:44:26 +0800

    在界面显示简单的文本元素

--- app/src/main/java/cn/com/uama/demo/hellocompose/MainActivity.kt
+++ app/src/main/java/cn/com/uama/demo/hellocompose/MainActivity.kt
@@ -2,32 +2,20 @@ package cn.com.uama.demo.hellocompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
-import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
+        下面的 setContent 块定义了当前 activity 的布局。
+        和以前使用 XML 文件定义布局不同，我们现在使用可组合函数来定义。
+        可组合函数就是用 @Composable 注解标记了的普通函数，在其中需要描述它所表示的组件需要如何展示在界面上。
+        Text() 就是一个在 Compose UI 库中定义的可组合函数，可以通过调用该函数在界面上显示一个文本元素。
+         */
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            Text(text = "Hello, Compose!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
     }
 }
